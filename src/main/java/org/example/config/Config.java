ba2fca5a4788d66be4caf9ca362221a5e3e86377
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2024-12-04 10:49:51 +0530

    new commit

---
+++ src/main/java/org/example/config/Config.java
@@ -0,0 +1,11 @@
+package org.example.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+    @Bean
+    public ModelMapper getMapper(){return new ModelMapper();}
+}
